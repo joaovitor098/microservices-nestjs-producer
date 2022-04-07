cf58b8d0f8619d48f57adb382c19d2a048c0a033
João Vítor Alves <joaovitoram01@hotmail.com>
2022-04-07 17:25:15 -0300

    feat: kafka client with microservice producer

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
 import { Controller, Get } from '@nestjs/common';
+import { Client, ClientKafka, Transport } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Client({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9093'],
+      },
+      consumer: {
+        groupId: 'test-consumer',
+      },
+    },
+  })
+  client: ClientKafka;
+
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  sendToStorage() {
+    return this.client.emit('storage-test', {
+      message: 'remove ice cream from storage',
+    });
   }
 }
